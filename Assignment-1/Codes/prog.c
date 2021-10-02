d4ea463fa27dd472fc3be9d839bc2b07d922bc13
shruti-chepuri <61222184+shruti-chepuri@users.noreply.github.com>
2021-10-02 11:26:04 +0530

    Create prog.c

---
+++ Assignment-1/Codes/prog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a=2,b=3,c=4,p,q,u=4,v=3;
+    p=a-b;
+    q=p*c;
+    p=u*v;
+    q+=p;
+    printf("p= %d \n", p);
+    printf("q= %d \n", q);
+    return 0;
+}
